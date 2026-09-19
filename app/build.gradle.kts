2691b54aba26adfe029d98ee63c8cc91b3ce1e5d
yaserghasemiopp-lab <yaser.ghasemiopp@gmail.com>
2026-09-19 23:58:13 +0330

    Configure Teshine Android package

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,14 +5,13 @@ plugins {
 }
 
 android {
-    namespace = "com.Your app name"
-    //noinspection GradleDependency
+    namespace = "com.ashrafi"
+
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.YOURAPPNAME"
+        applicationId = "com.ashrafi"
         minSdk = 24
-        //noinspection OldTargetApi
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
@@ -30,13 +29,16 @@ android {
             )
         }
     }
+
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+
     kotlinOptions {
         jvmTarget = "11"
     }
+
     buildFeatures {
         compose = true
     }
@@ -52,11 +54,13 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
+
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
 }
