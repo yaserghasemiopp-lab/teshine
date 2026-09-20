c60007792aa6cc7e9b360b49a0e3a3694fd70499
yaserghasemiopp-lab <yaser.ghasemiopp@gmail.com>
2026-09-20 12:25:24 +0330

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,16 +5,17 @@ plugins {
 }
 
 android {
-    namespace = "com.ashrafi"
+    namespace = "ir.teshine.app"
 
     compileSdk = 34
 
     defaultConfig {
-        applicationId = "com.ashrafi"
+        applicationId = "ir.teshine.app"
         minSdk = 24
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
+
         buildToolsVersion = "34.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
@@ -22,12 +23,14 @@ android {
 
     signingConfigs {
         create("release") {
+
             val keystorePath = System.getenv("TESHINE_KEYSTORE_PATH")
             val keystorePassword = System.getenv("TESHINE_KEYSTORE_PASSWORD")
             val keyAlias = System.getenv("TESHINE_KEY_ALIAS")
             val keyPassword = System.getenv("TESHINE_KEY_PASSWORD")
 
             if (!keystorePath.isNullOrBlank()) {
+
                 storeFile = file(keystorePath)
                 storePassword = keystorePassword
                 this.keyAlias = keyAlias
@@ -37,47 +40,75 @@ android {
     }
 
     buildTypes {
+
         release {
+
             isMinifyEnabled = false
+
             signingConfig = signingConfigs.getByName("release")
+
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
             )
         }
+
+        debug {
+            applicationIdSuffix = ".debug"
+            versionNameSuffix = "-debug"
+        }
     }
 
     compileOptions {
+
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
 
     kotlinOptions {
+
         jvmTarget = "11"
     }
 
     buildFeatures {
+
         compose = true
     }
 }
 
 dependencies {
+
     implementation("androidx.compose.foundation:foundation")
+
     implementation(libs.androidx.core.ktx)
+
     implementation(libs.androidx.lifecycle.runtime.ktx)
+
     implementation(libs.androidx.activity.compose)
+
     implementation(platform(libs.androidx.compose.bom))
+
     implementation(libs.androidx.compose.ui)
+
     implementation(libs.androidx.compose.ui.graphics)
+
     implementation(libs.androidx.compose.ui.tooling.preview)
+
     implementation(libs.androidx.compose.material3)
 
+
     testImplementation(libs.junit)
+
     androidTestImplementation(libs.androidx.junit)
+
     androidTestImplementation(libs.androidx.espresso.core)
+
     androidTestImplementation(platform(libs.androidx.compose.bom))
+
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
 
+
     debugImplementation(libs.androidx.compose.ui.tooling)
+
     debugImplementation(libs.androidx.compose.ui.test.manifest)
 }
